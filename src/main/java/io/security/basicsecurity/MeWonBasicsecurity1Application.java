66fdaa9ade545d95c337ab1ef00c2a7f6372fb09
Jeyun Park <jy095201@gmail.com>
2023-07-20 13:32:06 +0900

    init commit

---
+++ src/main/java/io/security/basicsecurity/MeWonBasicsecurity1Application.java
@@ -0,0 +1,13 @@
+package io.security.basicsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeWonBasicsecurity1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeWonBasicsecurity1Application.class, args);
+	}
+
+}
---
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -0,0 +1,7 @@
+package io.security.basicsecurity;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+}
